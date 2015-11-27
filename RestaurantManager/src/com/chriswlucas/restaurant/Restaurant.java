0111023babfceffd891f20e672203a1db9ccbf02
andersonn <njanderson2013@gmail.com>
2015-11-27 14:56:06 -0600

    Added methods to restaurant

--- RestaurantManager/src/com/chriswlucas/restaurant/Restaurant.java
+++ RestaurantManager/src/com/chriswlucas/restaurant/Restaurant.java
@@ -2,4 +2,8 @@ package com.chriswlucas.restaurant;
 
 class Restaurant {
 
+	
+	Worker getKitchen(){return;}; //needs to return the kitchen
+	Worker getBar() {return;}; //needs to return the bar.
+	static int getTicket () {return;}; //has to return a ticket number
 }

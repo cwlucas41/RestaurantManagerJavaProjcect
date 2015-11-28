e845d677497b2d77a7216eaf7a272c2ab7b30bdf
jbund93 <jonbundy93@gmail.com>
2015-11-28 16:16:43 -0600

    PaymentMgr methods

--- RestaurantManager/src/com/chriswlucas/restaurant/PaymentManager.java
+++ RestaurantManager/src/com/chriswlucas/restaurant/PaymentManager.java
@@ -1,5 +1,32 @@
 package com.chriswlucas.restaurant;
 
-class PaymentManager {
+import java.util.Scanner;
+import java.util.List;
 
+class PaymentManager {
+    
+    PaymentManager(){
+        
+    }
+    
+    
+    void checkout(int split){
+        for (int = 0; i<split; i++)
+        String output = "Enter customers for first ticket separated by a space:";
+        
+            
+    }
+    
+    void alertJobManager(){
+        
+    }
+    
+    void createReceipt(){
+        
+    }
+                   
+    
+                   
+                   
+    
 }
